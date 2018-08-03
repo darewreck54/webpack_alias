a5781d4e44059acbcf141d9ed7dedd857c95087d
Derek Hang <dehang@microsoft.com>
2018-08-03 13:32:46 -0700

    fix load issue

--- entity1/src/hello.tsx
+++ entity1/src/hello.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ally from 'ally.js';
+import * as loader from './loader';
 
 export const Hello2Component = () => {
   const a = ally.maintain.disabled({
     filter: '#test'
   });
-  
+  var test: any = loader.default;
   return (
     <div>
     <h2>EntryPoint 1 component !</h2>
